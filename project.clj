a6f3fbc1dc97857ad924f7e3a87160aee1215256
supersymmetry <maildumpert@gmail.com>
2013-07-23 11:13:51 +0200

    cleaned up some more

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject defrecord2 "1.0.1"
-  :description "Enhanced clojure records, personal fork from supersym."
+  :description "Enhanced clojure records, updated fork from supersym."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [matchure "0.10.1"]])
